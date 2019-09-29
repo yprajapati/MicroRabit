74f153c967985a40654c87ab2cc588e4b33b59eb
daksh-yogendra <yogendra@kandooit.co>
2019-09-29 23:01:43 +0530

    Initial Commit for Infra Bus and Infra IoC

---
+++ MicroRabit/MicroRabit.Infra.IoC/DependencyContainer.cs
@@ -0,0 +1,16 @@
+﻿using MicroRabit.Domain.Core.Bus;
+using MicroRabit.Infra.Bus;
+using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MicroRabit.Infra.IoC
+{
+    public class DependencyContainer
+    {
+        public static void RegisterServices(IServiceCollection services) {
+            services.AddTransient<IEventBus, RabitMQBus>();
+        }
+    }
+}
